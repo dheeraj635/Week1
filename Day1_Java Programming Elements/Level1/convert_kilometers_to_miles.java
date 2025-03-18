1e4b30c775315f1d05e8a0137010d0c4a87cf512
dheeraj0810 <dheerajreddy0810@gmail.com>
2025-03-18 12:00:00 +0530

    Deleting extras

--- Day1_Java Programming Elements/Level1/convert_kilometers_to_miles.java
+++
@@ -1,7 +0,0 @@
-public class convert_kilometers_to_miles {
-    public static void main(String[] args) {
-        double kilometers =10.8;
-        double miles = kilometers*0.621371;
-        System.out.println("The distance "+kilometers+" km in miles is "+miles);
-    }
-}
